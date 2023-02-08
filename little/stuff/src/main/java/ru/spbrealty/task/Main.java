11117e69956d6893e49ed8b3fd9dc82a9a702c27
SChepinog <chepinoga-sv@yandex.ru>
2023-02-08 13:38:22 +0300

    Test task for spb realty - perfect

--- little/stuff/src/main/java/ru/spbrealty/task/Main.java
+++ little/stuff/src/main/java/ru/spbrealty/task/Main.java
@@ -3,6 +3,8 @@ package ru.spbrealty.task;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,40 +12,30 @@ import java.util.stream.IntStream;
 public class Main {
     private final static int START_INCLUSIVE = 1;
     private final static int END_INCLUSIVE = 80;
+    private final static String EMPTY_CHAR = " ";
+    private final static Character NOT_EMPTY_CHAR = '#';
 
     public static void main(String[] args) throws Exception {
         DiamondSize diamondSize = getDiamondSizeFromConsole();
-//        printDiamond(5,5);
-//        printDiamond(6,3);
-//        printDiamond(6,6);
         printDiamond(diamondSize.height, diamondSize.width);
     }
 
     private static void printDiamond(int height, int width) {
-        char[] emptyChars = IntStream.range(0, width).mapToObj(i -> " ").collect(Collectors.joining("")).toCharArray();
-        int zeroPoint = width / 2;
-        Function<Integer, Integer> xWithCoefficient = x -> (int) ((((float) (width) / (height)) * x));
-        Function<Integer, Integer> xToRightIndex = x -> (width - 1) - Math.abs(xWithCoefficient.apply(x) - zeroPoint);
-        Function<Integer, Integer> xToLeftIndex = x -> Math.abs(zeroPoint - xWithCoefficient.apply(x));
+        char[] emptyChars = IntStream.range(0, width).mapToObj(i -> EMPTY_CHAR).collect(Collectors.joining()).toCharArray();
+        int heightIndexWithMaxWidth = new BigDecimal(height)
+                .divide(BigDecimal.valueOf(2), RoundingMode.UP)
+                .setScale(0, RoundingMode.UP).intValue() - 1;
+        Function<Integer, Integer> widthWithCoefficient = x -> (int) ((float) (width - 1) / heightIndexWithMaxWidth * x) + 1;
         for (int i = 0; i < height; i++) {
-            char[] clone = emptyChars.clone();
-            clone[xToRightIndex.apply(i)] = '#';
-            clone[xToLeftIndex.apply(i)] = '#';
-            System.out.println(clone);
+            printCharArrayWithGrids(emptyChars.clone(), widthWithCoefficient.apply(i <= heightIndexWithMaxWidth ? i : (height - i - 1)));
         }
-        System.out.println("----------------");
-        for (int i = 0; i < height; i++) {
-            char[] clone = emptyChars.clone();
-            clone[xToRightIndex.apply(i)] = '#';
-            System.out.println(clone);
-        }
-        System.out.println("----------------");
-        for (int i = 0; i < height; i++) {
-            char[] clone = emptyChars.clone();
-            clone[xToLeftIndex.apply(i)] = '#';
-            System.out.println(clone);
-        }
-        System.out.println("----------------");
+    }
+
+    private static void printCharArrayWithGrids(char[] emptyChars, int widthToPrint) {
+        int leftEmptyChars = emptyChars.length - ((emptyChars.length - widthToPrint) / 2) - widthToPrint;
+        emptyChars[leftEmptyChars] = NOT_EMPTY_CHAR;
+        emptyChars[leftEmptyChars + widthToPrint - 1] = NOT_EMPTY_CHAR;
+        System.out.println(emptyChars);
     }
 
     private static DiamondSize getDiamondSizeFromConsole() throws IOException {
