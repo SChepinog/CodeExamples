721af8a0ce622fe7c1c8a2ac46bfe5c29e4b3247
SChepinog <chepinoga-sv@yandex.ru>
2023-02-14 21:04:38 +0300

    TDD book  - 15 chapter

--- TddBook/src/main/java/dollar/Expression.java
+++ TddBook/src/main/java/dollar/Expression.java
@@ -2,4 +2,8 @@ package dollar;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+
+    Expression times(int rate);
+
+    Expression plus(Expression addend);
 }
--- TddBook/src/main/java/dollar/Sum.java
+++ TddBook/src/main/java/dollar/Sum.java
@@ -1,16 +1,25 @@
 package dollar;
 
 public class Sum implements Expression {
-    public Money augend;
-    public Money addend;
+    public Expression augend;
+    public Expression addend;
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
 
     public Money reduce(Bank bank, String to) {
-        int amount = augend.amount + addend.amount;
-        return new Money(amount, to);
+        return new Money(bank.reduce(augend, to).amount + bank.reduce(addend, to).amount, to);
+    }
+
+    @Override
+    public Expression times(int rate) {
+        return null;
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return null;
     }
 }
