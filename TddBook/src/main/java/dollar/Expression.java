ded5dee52b602fd752b09727ed626fd46ed8dc44
SChepinog <chepinoga-sv@yandex.ru>
2023-02-25 14:26:41 +0300

    TDD book  - 16 chapter

--- TddBook/src/main/java/dollar/Expression.java
+++ TddBook/src/main/java/dollar/Expression.java
@@ -3,7 +3,7 @@ package dollar;
 public interface Expression {
     Money reduce(Bank bank, String to);
 
-    Expression times(int rate);
+    Expression times(int multiplier);
 
     Expression plus(Expression addend);
 }
